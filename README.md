2d43a758dced9931cde57a03a518c4db2cb81467
Elena Scobici <elena@scobici.com>
2024-12-19 18:00:16 -0500

    Initital

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# OpenAiApi
+# OllamaSqlGen
+
+This is a small experimental projet which generates a SQL query using
+Meta's Llama 3.2 AI model.
+
+## Running
+
+Ensure that Ollama is installed. Then, run `ollama serve` to initialize
+the Ollama inference server.
+
+Navigate to the cloned directory and run `python sql_gen.py` to run
+the program.
---
+++ sql_gen.py
@@ -0,0 +1,28 @@
+import ollama
+
+schema = ""
+tableName = input("Enter table name (or DONE if done) >> ")
+
+while tableName != "DONE":
+    tableColumns = input("Enter the columns in the table " + tableName + " >> ")
+    schema += "There is a table named " + tableName + " with the columns: " + tableColumns + "."
+    tableName = input("Enter table name (or DONE if done) >> ")
+
+query = input("Enter the query >> ")
+print("Generating...\n")
+
+response = ollama.chat(
+    model="llama3.2:latest",
+    messages=[
+        {
+            "role": "user",
+            "content": "Consider the following SQL tables. "
+                + schema
+                + "Generate a SQL query for these tables for the following prompt: "
+                + query
+                + ". Don't include anything in your answer other than the query."
+                + "Your answer must include a SELECT and FROM clause, and other clauses if needed.",
+        },
+    ],
+)
+print(response["message"]["content"])
